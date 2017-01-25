f5feebd9dabbf9b7337cdec633ae22443f7eb259
Energy1190 <energyneo0@gmail.com>
2017-01-25 17:41:45 +0300

    +

--- reguests_db.py
+++ reguests_db.py
@@ -54,14 +54,15 @@ def get_route_info_database(*args, start_time=None, end_time=None, deep=4, **kva
     if start_time.timetuple()[2] != end_time.timetuple()[2]:
         ee = end_time.timetuple()
         ss = start_time.timetuple()
-        target = [['route', 'base-{0}-{1}-{2}'.format(j,h,i)] for i in range(ss[2],ee[2])
-                  for h in range(ss[1],ee[1])
-                  for j in range(ss[0],ee[0])]
+        target = [['route', 'base-{0}-{1}-{2}'.format(j,h,i)] for i in range(ss[2],ee[2]+1)
+                  for h in range(ss[1],ee[1]+1)
+                  for j in range(ss[0],ee[0]+1)]
     elif start_time.timetuple()[0:2] == datetime.datetime.now().timetuple()[0:2]:
-        target = target_collection
+        target = ['route', target_collection]
     else:
         ss = start_time.timetuple()
         target = ['route', 'base-{0}-{1}-{2}'.format(ss[0],ss[1],ss[2])]
+    print(target, start_time, end_time)
     t = get_time_requests(start_time, end_time, deep=deep)
     if t:
         for j in t:
@@ -85,7 +86,4 @@ def get_route_info_database(*args, start_time=None, end_time=None, deep=4, **kva
 if __name__ == '__main__':
     start_time = (2017, 1, 20, 22, 23)
     end_time = (2017, 1, 22, 22, 23)
-    print(type([]))
-    if type([]) == list:
-        print(get_time_requests(get_time_tuple('2017_1_22_22'),get_time_tuple('2017_2_22_22'), deep=2))
-#    print(get_route_info_database(**{'blle':'dsfsdf', 'flll':'sfadsf', 'start_time': (2033, 5, 12, 23, 56), 'end_time':None}))
+    print(get_route_info_database(**{'blle':'dsfsdf', 'flll':'sfadsf', 'start_time': '2017_1_23_01_56'}))
