05371a1a26e572cd24a36eb4b52f108f91185e91
Energy1190 <energyneo0@gmail.com>
2017-02-02 14:13:39 +0300

    +

--- requests_s.py
+++ requests_s.py
@@ -48,23 +48,46 @@ def return_nub(x):
     return int(''.join([i for i in x if isfloat(i)]))
 
 class Base():
+    """ Базовый класс для обработки входящих JSON-объектов. Включает в себя необходимые объекты:
+
+    * old - список со значениями, которые были заменены в результате обновления записи в
+    базе данных.
+    * dicts - словарь, который будет по умолчанию записан в базу данных.
+    * dst - коллекйия в базе данных, куда будет производиться запись, а так же откуда будут
+    извлекаться значения для проверки по умолчанию.
+    """
     def __init__(self, trg, target=None):
         self.old = []
         self.dicts = {}
         self.dst = target
 
     def encrypt( self, raw ):
+        """
+        Метод шифрования объектов в AES.
+        :param raw:
+        :return:
+        """
         iv = Random.new().read( AES.block_size )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
         return base64.b64encode( iv + cipher.encrypt( raw ) )
 
     def decrypt( self, enc ):
+        """
+        Метод дешифрования объектов из AES. Требует значение self.key
+        :param enc:
+        :return:
+        """
         enc = base64.b64decode(enc)
         iv = enc[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
         return cipher.decrypt( enc[16:] )
 
     def get_time_dict(self):
+        """
+        Метод для записи в словарь информации о времени. Разбитие времени на отдельные
+        элементы сделано для более удобного поиска по базе данных
+        :return:
+        """
         if 'time' in self.__dict__:
             if type(self.time) == time.struct_time:
                 self.dicts['time'] = time.strftime('%d.%m.%Y %H:%M:%S', self.time)
@@ -104,12 +127,28 @@ class Base():
                 db_update(self.dicts, target=self.dst, fild=dsttrg)
 
     def check_dict(self, target_dict):
+        """
+        Метод для проверки идентичности словарей. Исключает
+        значения _id (идентификационный номер записи в базе данных) и old
+        (список со старыми значениями). В случае обнаружения различий в
+        записях - записывает устаревшее значение в словарь old с указанием
+        времени сделаных изменений.
+        :param target_dict:
+        :return:
+        """
         for i in self.dicts:
             if i != '_id' and i != 'old' and i in target_dict:
                 if self.dicts[i] != target_dict[i]:
                     self.old.append({i: target_dict[i], 'time': datetime.datetime.now()})
 
     def get_dsttrg(self, src, fild):
+        """
+        Метод для получения объекта из целевой базы данных. Ищет объект по элементу
+        словаря ключ(fild) - значение(src)
+        :param src:
+        :param fild:
+        :return:
+        """
         x = db_get(src, target=self.dst, fild=fild)
         if x:
             return x
@@ -117,6 +156,9 @@ class Base():
             return False
 
 class Comp(Base):
+    """Класс обрабатывает информацию о компьютере на основании полученного от него
+    JSON-объекта.
+    """
     def __init__(self, trg, target=None):
         Base.__init__(self, trg, target=target)
         self.computername = trg['Userinfo']['Computername']
@@ -136,6 +178,17 @@ class Comp(Base):
         self.set_dict()
 
 class Dhcp(Base):
+    """Класс обрабатывает информацию от DHCP-сервера на основании полученного от него
+    JSON-объекта. Включает в себя:
+
+    * ip - ip-адрес выданный DHCP-сервером.
+    * mac - mac-адрес компьютера получившего ip-адрес.
+    * endtime - время истечения аренды ip-адреса.
+    * rezervation - статус резервирования ip-адреса.
+
+    На данный момент содержит объекты для шифрования/дешифрования входящих JSON-объектов, т.к.
+    на данный момент только они шифруются. В переспективе эти объекты будут перенесены в класс Base.
+    """
     def __init__(self, trg, target=None):
         Base.__init__(self, trg, target=target)
         self.key = base64.b64decode(trg['Key'])
@@ -144,6 +197,7 @@ class Dhcp(Base):
         self.timeinfo = time.gmtime((return_nub(self.dhcpinfo["Timeinfo"]) + 10800000)/1000.)
         self.dhcpinfo = self.dhcpinfo["Dhcpinfo"]
         self.dhcpinfo = self.generate_dict(self.dhcpinfo)
+
     def check_time(self, x):
         if x:
             return time.gmtime((return_nub(x) + 10800000)/1000.)
@@ -151,6 +205,9 @@ class Dhcp(Base):
             return x
 
     def generate_dict(self, x):
+        """Метод выводит более удобочитаемый список на основании полученного JSON-объекта.
+        И сортирует полученный список по IP-адресам.
+        """
         return sorted([{'name': x['name'][i],
                  'ip': x['ip'][i]['IPAddressToString'],
                  'mac': x['mac'][i],
@@ -164,6 +221,16 @@ class Dhcp(Base):
         del self.dicts['old']
 
 class User(Base):
+    """Класс обрабатывает информацию о пользователе на основании полученного от него
+    JSON-объекта. Включает в себя:
+
+    * username - имя пользователя, ввиду особенностей реализации необходимо переводить
+    в нижний регистр, что бы избежать записей-копий.
+    * domain - домен.
+    * computername - имя компьютера, на который был зафиксирован последний вход.
+    * time - время входа.
+    * copmslist - список со всеми зафиксированными входмаи/врменными метками
+    """
     def __init__(self, trg, target=None):
         Base.__init__(self, trg, target=target)
         self.username = str(trg['Userinfo']['Username']).lower()
@@ -238,6 +305,10 @@ class Route(Base):
             return False
 
 def delete_old_reqests(target, status='Old'):
+    """Процедура очистки устаревших/обработанных объектов в базе данных,
+    так же реализована возможность полного удаления коллекции базы данных,
+    при отсутствии именованного аргумента status.
+    """
     try:
         if status:
             trg = db_get(status, target=target, fild='Status')
@@ -249,6 +320,23 @@ def delete_old_reqests(target, status='Old'):
         pass
 
 def check_base(target):
+    """Процедура проверки коллекции базы данных. На данный момент реализована
+    только проверка коллекций users и copms. Ищет и удаляет пустые, не имеющие имени,
+    объекты из базы данных. Проверка осуществляется по одному ключу-значению в словаре
+    назначения. Предполагается, что объект не может быть правильным без этого значения.
+    Проверка осуществляется в нижнем регистре, чтобы измежать появления объектов копий.
+
+    Функция в зависимости от переданного ей аргумента, целевой базы коллекции данных,
+    определяет ключ для проверки сроваря. Если переданный аргумент не соответствует
+    ожиданиям функция вернет False. Аргумент должен быть итерабельным, как минимум из
+    двух элементов, в противном случае функция завершится с ошибкой.
+
+    После определения ключа для каждого элемента в коллекции идет его проверка на
+    не нулевое значение, а затем на соответствие с полученными ранее значениям, если
+    проверка пройдена проверяемый объект словаря добавляется в список y и будет участвовать
+    в последующих проверках, если хотя бы одна проверка не была пройдена проверяемый
+    элемент удаляется из коллекции.
+    """
     y = []
     x = db_find(target=target)
     if target[1] == 'comps':
@@ -303,7 +391,7 @@ def processing_incoming_json(target, out_target_users, out_target_comps, dhcp_ta
             x = Dhcp(t, target=dhcp_target)
             x.set_dict()
             for i in x.dicts:
-                y = x.get_dsttrg(i['name'])
+                y = x.get_dsttrg(i['name'], 'name')
                 if y and y['ip'] != i['ip']:
                     x.update(srctrg=i, dsttrg=y)
                 else:
