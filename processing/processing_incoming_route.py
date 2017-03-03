05eb44e92bfae7f63547aada3a4b45718d62fe7f
Energy1190 <energyneo0@gmail.com>
2017-03-03 16:49:25 +0300

    +

--- processing/processing_incoming_route.py
+++ processing/processing_incoming_route.py
@@ -2,18 +2,15 @@ from requests_s import Route, Vals, get_database_incoming
 from logmodule import logger
 
 def main(target, out_target):
-    try:
-        t = get_database_incoming(target, status=None)
-        if t:
-            x = Route(t, target=target)
-            y = x.set_dict()
-            if y:
-                Vals(y, target=['system', 'vals'])
-                x.set(x.dicts, target=out_target)
-                x.delete(t,target=target)
-                return True
-            else:
-                x.delete(t,target=target)
-                return False
-    except Exception as err:
-        logger.error('Error processing incoming log: {0}'.format(str(err)))
+    t = get_database_incoming(target, status=None)
+    if t:
+        x = Route(t, target=target)
+        y = x.set_dict()
+        if y:
+            Vals(y, target=['system', 'vals'])
+            x.set(x.dicts, target=out_target)
+            x.delete(t,target=target)
+            return True
+        else:
+            x.delete(t,target=target)
+            return False
