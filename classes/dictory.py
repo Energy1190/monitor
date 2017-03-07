5e4cb303551ca462dc63af4cfd1d83e52f20fb2f
Energy1190 <energyneo0@gmail.com>
2017-03-07 14:04:43 +0300

    +

--- classes/dictory.py
+++ classes/dictory.py
@@ -15,26 +15,36 @@ class Stat(Dictory):
             num /= 1024.0
         return "%.1f%s%s" % (num, 'Yi', suffix)
 
+    def __str__(self):
+        return str(self.trg)
+
+    def __getitem__(self, item):
+        return self.trg[item]
+
+    def get(self, obj):
+        return self.trg.get(obj)
+
     def __add__(self, other):
         try:
             if str(self.trg['ip']) == str(other['ip']):
                 self.trg['counts'] = int(self.trg['counts']) + int(other['counts'])
-                if self.trg['name'] != other['name']:
+                if other.get('name') and self.trg.get('name') != other['name']:
                     if self.trg.get('oldname'):
                         self.trg['oldname'].append({self.trg['name']: other['time']})
                     else:
                         self.trg['oldname'] = [{self.trg['name']: other['time']}]
                     self.trg['name'] = other['name']
-                if self.trg['user'] != other['user']:
+                if other.get('user') and self.get('user') != other['user']:
                     if self.trg.get('olduser'):
                         self.trg['olduser'].append({self.trg['user']: other['time']})
                     else:
                         self.trg['olduser'] = [{self.trg['user']: other['time']}]
                     self.trg['user'] = other['user']
-                self.trg['data']['in_bytes'] = self.trg['data']['in_bytes'] + other['data']['in_bytes']
-                self.trg['data']['out_bytes'] = self.trg['data']['out_bytes'] + other['data']['out_bytes']
-                self.trg['data']['in'] = self.sizeof_fmt(self.trg['data']['in_bytes'])
-                self.trg['data']['out'] = self.sizeof_fmt(self.trg['data']['out_bytes'])
+                if self.trg['data'].get('in_bytes') and self.trg['data'].get('out_bytes'):
+                    self.trg['data']['in_bytes'] = self.trg['data']['in_bytes'] + int(other['data'].get('in_bytes') or 0)
+                    self.trg['data']['out_bytes'] = self.trg['data']['out_bytes'] + int(other['data'].get('out_bytes') or 0)
+                    self.trg['data']['in'] = self.sizeof_fmt(self.trg['data']['in_bytes'])
+                    self.trg['data']['out'] = self.sizeof_fmt(self.trg['data']['out_bytes'])
                 return self.trg
         except:
             logger.error('Fail sum Statistics.')
