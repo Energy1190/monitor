088290c4863d17691ac9978e814a3cb9c4975806
Energy1190 <energyneo0@gmail.com>
2017-03-14 17:17:03 +0300

    +

--- web/auth.py
+++ web/auth.py
@@ -1,14 +1,25 @@
+import ldap3
 from functools import wraps
+from configuration import get_val
 from flask import request, Response, session
 
 logins = [{'name': 'admin', 'pass': 'secret'}]
+
+def get_ldap_info():
+    x = get_val('[LDAP]')
+    return [{'server': i['server'], 'domain': i['name']} for i in x]
+
+def ldap_auth(username, password):
+    server = ldap3.Server(get_ldap_info()[0]['server'])
+    conn = ldap3.Connection(server, user=str(get_ldap_info()[0]['domain']+ '\\' + username), password=password, authentication=ldap3.NTLM)
+    return conn.bind()
 
 def check_auth(username, password):
     for i in logins:
         if i['name'] == username:
             if i['pass'] == password:
                 return True
-    return False
+    return ldap_auth(username, password)
 
 def fail_auth():
     return Response('Could not verify your access level for that URL.\n'
