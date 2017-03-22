21b4434375c60777b6133e095af50ca219d5758e
Energy1190 <energyneo0@gmail.com>
2017-03-22 19:42:01 +0300

    +

--- command/clean.py
+++ command/clean.py
@@ -4,15 +4,16 @@
 
 from classes.db_mongo import Database
 
-t = Database(target=['clients','users']).fild()
-if t:
-    for i in list(t):
-        x = []
-        del i['_id']
-        for j in i['compslist']:
-            if j not in x:
-                x.append(j)
-            else:
-                print('Duplicate {0} in user {1}'.format(str(j), str(i['username'])))
-        i['compslist'] = x
-        Database(target=['clients','users'], fild='username', fild_var=i['username']).update(i)
+def main():
+    t = Database(target=['clients','users']).find()
+    if t:
+        for i in list(t):
+            x = []
+            del i['_id']
+            for j in i['compslist']:
+                if j not in x:
+                    x.append(j)
+                else:
+                    print('Duplicate {0} in user {1}'.format(str(j), str(i['username'])))
+            i['compslist'] = x
+            Database(target=['clients','users'], fild='username', fild_var=i['username']).update(i)
