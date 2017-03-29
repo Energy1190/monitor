53ef0919a8125a75b22735203d8a50ce05567cfd
Energy1190 <energyneo0@gmail.com>
2017-03-29 14:34:51 +0300

    +

--- system/logmodule.py
+++ system/logmodule.py
@@ -1,10 +1,11 @@
+import sys
 import logging
 
 logger = logging.getLogger('main_logs')
 logger.setLevel(logging.DEBUG)
 fh = logging.FileHandler('logging.log')
 fh.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
+ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
