4b498a3e6c30b1fb3370b235ef63b154c5ffb820
Energy1190 <energyneo0@gmail.com>
2017-03-29 18:24:32 +0300

    +

--- start_dally_statistics.py
+++ start_dally_statistics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #  -*- coding: utf-8 -*-
 
-from processing.processing_statistics_route import processing_statistics_route
+from processing.processing_statistics_route import main as processing_statistics_route
 from processing.processing_maintenance import main as processing_maintenance
 
 if __name__ == '__main__':
