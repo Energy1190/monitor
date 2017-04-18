dc275d46e501ab1ad6fd2a42503ccb60c61df72f
Energy1190 <energyneo0@gmail.com>
2017-04-18 17:33:51 +0300

    +

--- start_webapp.py
+++ start_webapp.py
@@ -7,7 +7,6 @@ from system.system import watch_pid as check
 
 def main():
     print('Start web interface')
-    check()
     app.debug = True
     app.secret_key = 'not-secret-key'
     app.run(port=5000, host='0.0.0.0')
